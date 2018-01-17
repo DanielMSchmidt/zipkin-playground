98002c427bd55b98abb5f2cdd34d002d643023a3
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-01-17 21:35:53 +0100

    add zipkin tracing

--- id/index.js
+++ id/index.js
@@ -1,8 +1,34 @@
+// Load environment from dotfile
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const {
+  Tracer,
+  BatchRecorder,
+  jsonEncoder: { JSON_V2 }
+} = require('zipkin');
+const { HttpLogger } = require('zipkin-transport-http');
+const CLSContext = require('zipkin-context-cls');
+const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
 
+const zipkinHostPort = process.env.ZIPKIN_HOST_PORT || 'http://localhost:9411';
+const serviceName = process.env.SERVICE || 'pleaseEnterServiceName';
+
+const app = express();
 app.use(bodyParser.json());
+
+
+const recorder = new BatchRecorder({
+  logger: new HttpLogger({
+    endpoint: zipkinHostPort + '/api/v2/spans',
+    jsonEncoder: JSON_V2,
+    httpInterval: 1
+  })
+});
+const tracer = new Tracer({ ctxImpl: new CLSContext('zipkin'), recorder, localServiceName: serviceName });
+app.use(zipkinMiddleware({ tracer }));
+
 app.get('/health', (req, res) => {
   res.status(200);
   res.end();
